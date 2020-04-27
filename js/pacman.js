d6c155f5db405db47f74d4df421ef3f357a8b0d5
Fabien Sourdon <fabien.sourdon@gmail.com>
2020-04-27 14:07:46 +0200

    ModifFonctionStart

--- js/pacman.js
+++ js/pacman.js
@@ -23,27 +23,35 @@ let level = [
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ];
 
-for(var i in level){
+start();
+
+function start()
+{
     let elemUp = document.getElementById('contDiv');
-    let maDiv = document.createElement('div');
-    maDiv.className = 'ligne';
-    
+    elemUp.innerHTML = " ";
 
-    for (var j in level[i]){
-        let maDivIn = document.createElement('div');
+    for(var i in level){
+        for (var j in level[i]){
+            
+            let maDivIn = document.createElement('div');
 
-        if (level[i][j] === 0){
-            maDivIn.className = 'mur';
+            if (level[i][j] === 0){
+                maDivIn.className = 'mur';
+            }
+            else if (level[i][j] === 1){
+                maDivIn.className = 'sol';
+            }
+            else if (level[i][j] === 2){
+                maDivIn.className = 'gum';
+            }   
+            elemUp.appendChild(maDivIn);
         }
-        else if (level[i][j] === 1){
-            maDivIn.className = 'sol';
-        }
-        else if (level[i][j] === 2){
-            maDivIn.className = 'gum';
-        }   
-        elemUp.appendChild(maDivIn);
     }
-    
-    
+}
+
+function refresh()
+{
+// là on mets le code a afficher toutes les secondes
+  setTimout(refresh, 1000)
 }
 
